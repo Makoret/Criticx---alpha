e84b6d7bf263900d7a6dfa584644d7512ce78cc0
FrankFlores <fraco.1918@gmail.com>
2020-05-26 17:24:25 -0500

    callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,3 @@
 class User < ApplicationRecord
      has_many :reviews
-     after_create :after_create do
-          
-     end
-
-     private
-     def count_up
-          if self.user.review_count.nil? self.user.update(review_count:0)
-          end
-          count = self.user.review_count
-          self.user.update(review_count: count + 1)
-     end
-     def count_down
-          count = self.user.review_count
-          self.user.update(review_count: count + 1)
-     end
 end
