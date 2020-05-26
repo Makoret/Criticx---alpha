5680bdad837bfdbfe08e5ddb4d4110bba5ee3b98
FrankFlores <fraco.1918@gmail.com>
2020-05-25 19:24:29 -0500

    criticx models & associations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+     has_many :involved_company
+     has_many :games, through: :involved_company
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ApplicationRecord
+     has_many :involved_company
+     has_many :companies, through: :involved_company
+     has_and_belongs_to_many :platforms
+     has_and_belongs_to_many :genres
 end
