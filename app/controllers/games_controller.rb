5a838a263b37ccc1dc5e92817b3ce8e8b82323b0
FrankFlores <fraco.1918@gmail.com>
2020-05-27 23:32:14 -0500

    added edit option to the items

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,6 +16,17 @@ class GamesController < ApplicationController
       render :new
     end
   end
+  def edit
+    @game = Game.find(params[:id])
+  end
+  def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to(@game)
+    else
+      render :edit
+    end
+  end
   
   private
   
